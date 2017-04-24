235368194079c732734c721ba6ea4bd0dc6dc1ea
Acintya <lingsheng.hs@gmail.com>
2017-04-24 23:52:48 +0200

    comment

--- ann_tf_Tensorboard.py
+++ ann_tf_Tensorboard.py
@@ -3,9 +3,8 @@ import tensorflow as tf
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
     # add one more layer and return the output of this layer
-    # 区别：大框架，定义层 layer，里面有 小部件
+    # difference：name_scope under add_layer
     with tf.name_scope('layer'):
-        # 区别：小部件
         with tf.name_scope('weights'):
             Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
         with tf.name_scope('biases'):
@@ -20,7 +19,7 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
 
 
 # define placeholder for inputs to network
-# 区别：大框架，里面有 inputs x，y
+# inputs x，y
 with tf.name_scope('inputs'):
     xs = tf.placeholder(tf.float32, [None, 1], name='x_input')
     ys = tf.placeholder(tf.float32, [None, 1], name='y_input')
@@ -31,20 +30,20 @@ l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
 prediction = add_layer(l1, 10, 1, activation_function=None)
 
 # the error between prediciton and real data
-# 区别：定义框架 loss
+# define frame loss
 with tf.name_scope('loss'):
     loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
                                         reduction_indices=[1]))
 
-# 区别：定义框架 train
+# define frame train
 with tf.name_scope('train'):
     train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 sess = tf.Session()
 
-# 区别：sess.graph 把所有框架加载到一个文件中放到文件夹"logs/"里 
-# 接着打开terminal，进入你存放的文件夹地址上一层，运行命令 tensorboard --logdir='logs/'
-# 会返回一个地址，然后用浏览器打开这个地址，在 graph 标签栏下打开
+# sess.graph load all frames in one file under "logs/"
+# in terminal: in the root, run: tensorboard --logdir='logs/'
+# open adress in browser: tab graph
 writer = tf.train.SummaryWriter("logs/", sess.graph)
 # important step
 sess.run(tf.initialize_all_variables())
